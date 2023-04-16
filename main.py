5ebf4fa8b253f07c303e51d05457578ed2f0358f
Jim-Qian <122236062+JimQian2023@users.noreply.github.com>
2023-04-16 13:20:27 -0400

    2nd Commit (plot_implicit.py and plot.py) (plot_implicit() accepts label and legend now, but graphs on ylabel instead of legend)

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
+# import matplotlib.pyplot as plt
+# from sympy import plot_implicit, symbols, Eq, And
+
+# x, y = symbols('x y')
+# p1 = plot_implicit(Eq(x**2 + y**2, 5), backend='matplotlib')
+# plt.show()
+
 import matplotlib.pyplot as plt
 from sympy import plot_implicit, symbols, Eq, And
 
+from sympy import plot
+from sympy.plotting.plot import Plot
+from sympy import Symbol
+
 x, y = symbols('x y')
-p1 = plot_implicit(Eq(x**2 + y**2, 5), backend='matplotlib')
-plt.show()
+
+p = Plot(x**2, title='Squared Plot', label='foo', legend=True, show=True)
+p.show()
